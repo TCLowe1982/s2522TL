568eb96ed6f3eec79f20b41777f36a02a191198c
TCLowe1982 <111537227+TCLowe1982@users.noreply.github.com>
2025-04-02 11:50:31 -0500

    Connect String
    I think this is in the right place.

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const Router = require("./routes")
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect("mongodb+srv://holyknight03:<db_password>@learnmongo.ce5vwcc.mongodb.net/?retryWrites=true&w=majority&appName=LearnMongo",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
+
